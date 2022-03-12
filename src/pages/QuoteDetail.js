26be195ce5f34fc62317a8d731212aef2d9d3cea
smadjapa <smadjapa@gmail.com>
2022-03-12 19:45:56 +0300

    Adding main layout and nested routing

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,6 +1,18 @@
+import { Fragment } from 'react';
+import { Route } from 'react-router-dom';
+import { useParams } from "react-router-dom";
+import Comments from '../components/comments/Comments';
+
 const QuoteDetail = () => {
+    const params = useParams();
     return(
-        <h1>Quote Detail Page</h1>
+        <Fragment>
+            <h1>Quote Detail Page</h1>
+            <p>{ params.quoteId }</p>
+            <Route path={`/quotes/${params.quoteId}/comments`}>
+                <Comments />
+            </Route>
+        </Fragment>
     );
 };
 
